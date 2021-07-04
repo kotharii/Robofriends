d6a790a591145da77cc7b3977684f58b16ef4967
Naman Kothari <namanjuly101@gmail.com>
2021-07-04 21:24:11 +0530

    shifted from classes to functions even with state by using 'useState','useEffect'

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React,{Component} from "react";
+import React,{ useState, useEffect } from "react";
 import Cardlist from "../components/cardlist";
 // import robots from './robots';
 import Searchbox from "../components/Searchbox";
@@ -6,47 +6,47 @@ import "./Apps.css";
 import Scroll from '../components/Scroll'
 // import Errorboundry from "../components/Errorboundry.js"
 
-class App extends Component {
-    constructor(){
-      super()
-        this.state = {
-            robots: [],
-            searchfield: '',
-        }
-    }
+const App = () => {
+    // constructor(){
+    //   super()
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: '',
+    //     }
+    // }
 
-    componentDidMount(){
+    const [ robots , setrobots ] = useState([]);
+    const [ searchfield , setsearchfield ] = useState('');
+    
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users').then(response =>response.json())
-        .then(users => this.setState({ robots : users }))
-    }
+        .then(users => {setrobots(users)});
+    },[])   // the "[]" is the stopping condition...if you enter "[robots]" then it means that 
+    // "it will keep on running the 'useEffect function' until the robots state will keep on changing"
    
-    onsearchchange = (event) => {
-        this.setState({ searchfield: event.target.value })
+    const onsearchchange = (event) => {
+        setsearchfield (event.target.value)
     }
 
-    // filteredrobots=()=>{
-    //     searchfield.onchange.Tolowercase().includes()
-    // };
-    render(){
-        const filteredrobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-        })   
-        // this.state.robots.length === 0 is the same as !this.state.robots.length as 
-        // ! is not so it will return true and go in the if case automatically
-        return !this.state.robots.length ?
-        (<h1> Loading ... </h1>) :
-        (
-            <div className="tc">
-                    
-                <h1 className="f1">Robofriends</h1>
-                <Searchbox searchfield={this.state.searchfield} searchchange={this.onsearchchange}/>
-                <Scroll>
-                        <Cardlist robots={filteredrobots}/>
-                    
-                </Scroll>
-            </div>
-        );
-    };    
+        // filteredrobots=()=>{
+        //     searchfield.onchange.Tolowercase().includes()
+        // };
+    const filteredrobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })   
+    // this.state.robots.length === 0 is the same as !this.state.robots.length as 
+    // ! is not so it will return true and go in the if case automatically
+    return !robots.length ?
+    (<h1> Loading ... </h1>) :
+    (
+        <div className="tc">
+                
+            <h1 className="f1">Robofriends</h1>
+            <Searchbox searchchange={onsearchchange}/>
+            <Scroll>
+                    <Cardlist robots={filteredrobots}/>
+            </Scroll>
+        </div>
+    );    
 }
 export default App;
-// hello saxy baby
